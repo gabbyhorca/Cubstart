5f5ebba97cabeac8c1a450f583a9fd72d8e3be88
ghorca <ghorca@berkeley.edu>
2023-03-02 14:50:30 -0800

    add quantity

--- CoffeeShopMenu/CoffeeShopMenu/ContentView.swift
+++ CoffeeShopMenu/CoffeeShopMenu/ContentView.swift
@@ -34,7 +34,7 @@ class Coffee: ObservableObject, Identifiable {
 struct ContentView: View {
     //PART 1B: Initialize objects of the Coffee class aka "menu items" below
    @StateObject var latte = Coffee(itemNum: 1, drinkName: "I love you a latte", drinkType: "Latte", price: 3.75, quantity: 0)
-   @StateObject var espresso = Coffee(itemNum: 2, drinkName: "No Longer Espresso Depresso", drinkType: "Espresso", price: 3.25, quantity: 0)
+   @StateObject var espresso = Coffee(itemNum: 2, drinkName: "Not Espresso Depresso", drinkType: "Espresso", price: 3.25, quantity: 0)
    @StateObject var mocha = Coffee(itemNum: 3, drinkName: "Loca for Mocha", drinkType: "Mocha", price: 4.00, quantity: 0)
    @StateObject var coldBrew = Coffee(itemNum: 4, drinkName: "Brew-tiful Day", drinkType: "Cold Brew", price: 3.00, quantity: 0)
     
@@ -105,11 +105,11 @@ struct ContentView: View {
                          */
                         
                         //PART 2B: Display the quantity of each item
-                        /*
+                        
                         VStack(alignment: .leading) {
-                            Text(String([INSERT QUANTITY]))
+                           Text(String(i.quantity))
                         } .padding(.bottom, 20)
-                        */
+                        
                 
                         //PART 3A: IMPLEMENT YOUR INCREMENT BUTTON IN THE SPACE BELOW
 
