fd986c02841a36163e9afa43775a52b8357b7763
ghorca <ghorca@berkeley.edu>
2023-03-13 01:39:54 -0700

    part 1 and indentation fix

--- Maze_Game-HW/hw4/hw4/ContentView.swift
+++ Maze_Game-HW/hw4/hw4/ContentView.swift
@@ -8,48 +8,57 @@ import SwiftUI
 
 /* Just for placeholder data type. Not useful. */
 enum DummyDataType {
-    case dummy
+   case dummy
 }
 
 struct ContentView: View {
-    // The navigationStack control point. If appended to, it will navigate the user to a new view.
-    @State var navPath = NavigationPath()
-    
-    // TODO: Uncomment solutionPath and initialize it to the correct value.
-    // Array of strings representing the path to the maze exit.
-    // @State var solutionPath: [String]
-    
-    // Array of strings representing the current path the user has taken.
-    @State var currentPath: [String] = []
-    // String representation of currentPath
-    @State var currentPathString: String = ""
-    // String representation of solutionPath. Helpful for debugging and testing.
-    @State var solutionPathString: String = ""
-    
-    var body: some View {
-        // NavigationStack wraps entire view.
-        NavigationStack(path: $navPath) {
-            VStack {
-                // TODO: Menacing Maze Text Warning
-                
-                // TODO: Begin game Button
+   // The navigationStack control point. If appended to, it will navigate the user to a new view.
+   @State var navPath = NavigationPath()
+   
+   // TODO: Uncomment solutionPath and initialize it to the correct value.
+   // Array of strings representing the path to the maze exit.
+   @State var solutionPath: [String] = generate_turns(size: 3)
+   
+   // Array of strings representing the current path the user has taken.
+   @State var currentPath: [String] = []
+   // String representation of currentPath
+   @State var currentPathString: String = ""
+   // String representation of solutionPath. Helpful for debugging and testing.
+   @State var solutionPathString: String = ""
+   
+   var body: some View {
+      // NavigationStack wraps entire view.
+      NavigationStack(path: $navPath) {
+         VStack {
+            // TODO: Menacing Maze Text Warning
+            Text("☠️ Enter the maze at your own risk! ☠️")
+            // TODO: Begin game Button
+            // Append to navigationStack when button is pressed.
+            Button("Begin Maze") {
+               navPath.append("Begin Maze")
             }
-            .navigationTitle("Maze Game")
-            // TODO: Replace DummyDataType with correct data type.
-            .navigationDestination(for: DummyDataType.self) { action in
-                // TODO: Replace EmptyView() with code to display correct view based on action pushed onto the navigation stack.
-                EmptyView()
+            .padding()
+         }
+         .navigationTitle("Maze Game")
+         // TODO: Replace DummyDataType with correct data type.
+         .navigationDestination(for: String.self) { action in
+            // TODO: Replace EmptyView() with code to display correct view based on action pushed onto the navigation stack.
+            if (solutionPathString == currentPathString) {
+               MazeExitView(navPath: $navPath, currentPath: $currentPath, solutionPath: $solutionPath, solutionPathString: $solutionPathString, currentPathString: $currentPathString)
+            } else {
+               MazeLocationView(previous_action: action, navPath: $navPath, currentPath: $currentPath, solutionPath: $solutionPath, solutionPathString: $solutionPathString, currentPathString: $currentPathString)
             }
-        }
-        .onAppear {
-            // TODO: Uncomment after initializing solutionPath
-            // solutionPathString = solutionPath.joined(separator: "->")
-        }
-    }
+         }
+      }
+      .onAppear {
+         // TODO: Uncomment after initializing solutionPath
+         // solutionPathString = solutionPath.joined(separator: "->")
+      }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
--- Maze_Game-HW/hw4/hw4/MazeLocationView.swift
+++ Maze_Game-HW/hw4/hw4/MazeLocationView.swift
@@ -9,57 +9,71 @@ import SwiftUI
 
 /* A view that is presented when a player is somewhere in the maze. */
 struct MazeLocationView: View {
-    // The previous action taken to get here.
-    @State var previous_action: String
-    
-    // See ContentView for variable descriptions.
-    @Binding var navPath: NavigationPath
-    @Binding var currentPath: [String]
-    @Binding var solutionPath: [String]
-    @Binding var solutionPathString: String
-    @Binding var currentPathString: String
-    
-    /* If false, user is moving forward into this location.
-     If true, the next time this location is visited, it will be considered a backtrack-move.
-     */
-    @State var backtrack: Bool = false
-    
-    
-    var body: some View {
-        VStack {
-            // TODO: Display message that user has entered the maze OR their previous move and current path.
+   // The previous action taken to get here.
+   @State var previous_action: String
+   
+   // See ContentView for variable descriptions.
+   @Binding var navPath: NavigationPath
+   @Binding var currentPath: [String]
+   @Binding var solutionPath: [String]
+   @Binding var solutionPathString: String
+   @Binding var currentPathString: String
+   
+   /* If false, user is moving forward into this location.
+    If true, the next time this location is visited, it will be considered a backtrack-move.
+    */
+   @State var backtrack: Bool = false
+   
+   
+   var body: some View {
+      VStack {
+         // TODO: Display message that user has entered the maze OR their previous move and current path.
+         if (previous_action == "Begin Maze") {
+            Text("You have entered the maze.")
+         } else {
+            Text("Your move: \(previous_action).")
+               .padding(.bottom, 10)
             
-            Spacer()
+            Text("Your current path: \(currentPathString)")
+         }
+         
+         Spacer()
+         
+         // TODO: Text that makes player nervious about picking their next move.
+         Text("Pick your next move with caution.")
+            .padding()
+         // TODO: Explore Up Button + actions
+         Button("Up") {
+            backtrack = true
+            currentPath.append("Up")
+            navPath.append("Up")
+         }
+         .padding()
+         HStack {
+            // TODO: Explore Left Button
             
-            // TODO: Text that makes player nervious about picking their next move.
-            
-            // TODO: Explore Up Button + actions
-            
-            HStack {
-                // TODO: Explore Left Button
-                
-                // TODO: Explore Right Button
-            }
-            
-            // TODO: Explore Down Button
-            
-            Spacer()
-            Spacer()
-            
-            // Display the answer at the bottom for debugging/testing purposes.
-            Text(solutionPath.joined(separator: "->"))
-        }
-        .onAppear {
-            // TODO: Implement Backtracking
-            
-            // Refresh the currentPathString variable to reflect new current path whether visiting or backtracking.
-            currentPathString = currentPath.joined(separator: "->")
-        }
-    }
+            // TODO: Explore Right Button
+         }
+         
+         // TODO: Explore Down Button
+         
+         Spacer()
+         Spacer()
+         
+         // Display the answer at the bottom for debugging/testing purposes.
+         Text(solutionPath.joined(separator: "->"))
+      }
+      .onAppear {
+         // TODO: Implement Backtracking
+         
+         // Refresh the currentPathString variable to reflect new current path whether visiting or backtracking.
+         currentPathString = currentPath.joined(separator: "->")
+      }
+   }
 }
 
 struct MazeLocationView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
