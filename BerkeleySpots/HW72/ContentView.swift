9d55bf135610a836594b33e3acdf58f1cfcf030a
ghorca <ghorca@berkeley.edu>
2023-04-06 18:43:51 -0700

    finish hw7

--- BerkeleySpots/HW72/ContentView.swift
+++ BerkeleySpots/HW72/ContentView.swift
@@ -9,35 +9,49 @@ import SwiftUI
 import Firebase
 import FirebaseCore
 
+struct SuccessScreen: View {
+   var body: some View {
+      Text("Success!")
+   }
+}
+
 struct ContentView: View {
     @State var email = ""
     @State var password = ""
     //TODO: add a variable that tracks whether the user has successfully logged in or not
+   @State var logInSuccess = false;
     
     init() {
         FirebaseApp.configure()
     }
     
     func login() {
-        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if error != nil {
-                print(error?.localizedDescription ?? "")
-            } else {
-//                TODO: update the variable to track that the user has successfully logged in
-                print("success")
-            }
-        }
+       Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+          if error != nil {
+             print(error?.localizedDescription ?? "")
+          } else {
+             //                TODO: update the variable to track that the user has successfully logged in
+             logInSuccess = true;
+             print("success")
+          }
+       }
     }
+   
 
     var body: some View {
         //TODO: Implement an if-else to return a new view when a certain condition is met
-        TextField("Email", text: $email).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
-            TextField("Password", text: $password).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
-                
-            Button(action: { login() }){
-                     Text("Sign in")
-                 }.buttonStyle(. bordered).tint(.mint)
-        }
+       if (logInSuccess) {
+          SuccessScreen()
+       }
+       else {
+          TextField("Email", text: $email).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
+              TextField("Password", text: $password).textFieldStyle(.roundedBorder).multilineTextAlignment(.center)
+                  
+              Button(action: { login() }){
+                       Text("Sign in")
+                   }.buttonStyle(. bordered).tint(.mint)
+          }
+       }
 }
 
 struct ContentView_Previews: PreviewProvider {
